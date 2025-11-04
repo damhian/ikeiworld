8cb0640bdc4043349a0525caf853c2354d45d2a3
damhian <david.mayshiogie@protonmail.com>
2025-11-04 19:13:55 +0700

    add homepage section, friend card, no friend card, no recommended users

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,25 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router";
+
 import {
+  CheckCircleIcon,
+  MapPinIcon,
+  UserPlusIcon,
+  UsersIcon,
+} from "lucide-react";
+
+import {
+  getOutgoingFriendsReqs,
   getRecommendedUsers,
   getUsersFriends,
   sendFriendRequest,
 } from "../lib/api";
+import { capitalize } from "../lib/utils";
+
+import FriendCard, { getLanguageFlag } from "../components/FriendCard";
+import NoFriendsFound from "../components/NoFriendsFound";
+import NoRecommendedUser from "../components/NoRecommendedUser";
 
 const HomePage = () => {
   const queryClient = useQueryClient();
@@ -35,12 +50,130 @@ const HomePage = () => {
     const outgoingIds = new Set();
     if (outgoingFriendReqs && outgoingFriendReqs.length > 0) {
       outgoingFriendReqs.forEach((req) => {
-        outgoingIds.add(req.id);
+        outgoingIds.add(req.recipient._id);
       });
+      setOutgoingRequestsIds(outgoingIds);
     }
   }, [outgoingFriendReqs]);
 
-  return <div>HomePage</div>;
+  return (
+    <div className="p-4 sm:p-6 lg:p-8">
+      <div className="container mx-auto space-y-10">
+        <div className="flex flex-col sm:flex-row items-start sm:items-center justify-between gap-4">
+          <h2 className="text-2xl sm:text-3xl font-bold tracking-tight">
+            Your Friends
+          </h2>
+          <Link to="/notifications" className="btn btn-outline btn-sm">
+            <UsersIcon className="mr-2 size-4" />
+            Friend Requests
+          </Link>
+        </div>
+
+        {loadingFriends ? (
+          <div className="flex justify-center py-12">
+            <span className="loading loading-spinner loading-lg"></span>
+          </div>
+        ) : friends.length === 0 ? (
+          <NoFriendsFound />
+        ) : (
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+            {friends.map((friend) => (
+              <FriendCard key={friend._id} friend={friend} />
+            ))}
+          </div>
+        )}
+
+        <section>
+          <div className="mb-6 sm:mb-8">
+            <div className="flex flex-col sm:flex-row items-start sm:items-center justify-between gap-4">
+              <div>
+                <h2 className="text-2xl sm:text-3xl font-bold tracking-tight">
+                  Meet New Collaborators
+                </h2>
+                <p className="opacity-70">
+                  Discover new partner to collaborate based on your profile
+                </p>
+              </div>
+            </div>
+          </div>
+
+          {loadingUsers ? (
+            <div className="flex justify-center py-12">
+              <span className="loading loading-spinner loading-lg"></span>
+            </div>
+          ) : recommendedUsers.length === 0 ? (
+            <NoRecommendedUser />
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+              {recommendedUsers.map((user) => {
+                const hasRequestBeenSent = outgoingRequestsIds.has(user._id);
+
+                return (
+                  <div className="card bg-base-200 hover:shadow-lg transition-all duration-300">
+                    <div className="card-body p-5 space-y-4">
+                      <div className="flex items-center gap-3">
+                        <div className="avatar size-16 rounded-full">
+                          <img src={user.profilePic} alt={user.fullName} />
+                        </div>
+
+                        <div>
+                          <h3 className="font-semibold text-lg">
+                            {user.fullName}
+                          </h3>
+                          {user.location && (
+                            <div className="flex items-center text-xs opacity-70 mt-1">
+                              <MapPinIcon className="size-3 mr-1" />
+                              {user.location}
+                            </div>
+                          )}
+                        </div>
+                      </div>
+
+                      {/* Languages with Flags */}
+                      <div className="flex flex-wrap gap-1.5">
+                        <span className="badge badge-secondary">
+                          {getLanguageFlag(user.nativeLanguage)}
+                          Native: {capitalize(user.nativeLanguage)}
+                        </span>
+                        <span className="badge badge-outline">
+                          {getLanguageFlag(user.learningLanguage)}
+                          Learning: {capitalize(user.learningLanguage)}
+                        </span>
+                      </div>
+
+                      {user.bio && (
+                        <p className="text-sm opacity-70"> {user.bio}</p>
+                      )}
+
+                      {/* Friend Request Button */}
+                      <button
+                        className={`btn w-full mt-2 ${
+                          hasRequestBeenSent ? "btn-disabled" : "btn-primary"
+                        }`}
+                        onClick={() => sendRequestMutation(user._id)}
+                        disabled={hasRequestBeenSent || isPending}>
+                        {hasRequestBeenSent ? (
+                          <>
+                            <CheckCircleIcon className="size-4 mr-2" />
+                            Request Sent
+                          </>
+                        ) : (
+                          <>
+                            <UserPlusIcon className="size-4 mr-2" />
+                            Send Friend Request
+                          </>
+                        )}
+                      </button>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          )}
+        </section>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
